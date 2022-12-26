db25e88fe83b4288b9941b690b6feb61746ad9c8
cuksmokey <bolongs360@gmail.com>
2022-12-26 18:31:28 +0700

    edit dikit v stok nya

--- application/views/Laporan/v_stok.php
+++ application/views/Laporan/v_stok.php
@@ -574,4 +574,21 @@
 		});
 	}
 
+	//
+
+	function hanyaAngka(evt) {
+		var charCode = (evt.which) ? evt.which : event.keyCode
+		if (charCode > 31 && (charCode < 48 || charCode > 57))
+			return false;
+		return true;
+	}
+
+	function aK(evt) {
+		var charCode = (evt.which) ? evt.which : event.keyCode
+		// alert(charCode);
+		if (charCode > 31 && (charCode < 46 || charCode > 57))
+			return false;
+		return true;
+	}
+
 </script>
