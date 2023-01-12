9cef888b21a8ce893c7f9a09967d6656304a524a
cuksmokey <bolongs360@gmail.com>
2023-01-12 13:43:28 +0700

    penyesuain cari input roll sementara ppi hilangkan

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -3131,7 +3131,7 @@ class Master extends CI_Controller
 		$getRoll = $this->db->query("SELECT*FROM m_timbangan
 		WHERE nm_ker='$nm_ker' AND g_label='$g_label' AND width='$width' AND roll LIKE '%$roll%'
 		AND tgl BETWEEN '2020-04-01' AND '9999-01-01'
-		AND (status='0' OR status='2' OR status='3') AND id_pl='0' AND (id_rk IS NULL OR id_rk = '')
+		AND (status='0' OR status='3') AND id_pl='0' AND (id_rk IS NULL OR id_rk = '')
 		ORDER BY pm,roll");
 		if($getRoll->num_rows() == 0){
 			$html .='<div class="notfon">DATA TIDAK DITEMUKAN</div>';
